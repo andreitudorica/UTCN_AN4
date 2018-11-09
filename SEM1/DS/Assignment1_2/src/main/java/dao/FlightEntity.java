936325e419be7f4f10d800a0177bd085c05b7554
abora13 <abora@macadamian.com>
2018-11-09 21:00:37 +0200

    9_11_2017

---
+++ SEM1/DS/Assignment1_2/src/main/java/dao/FlightEntity.java
@@ -0,0 +1,90 @@
+package dao;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "flight", schema = "assignment1_2", catalog = "")
+public class FlightEntity {
+    private int id;
+    private int number;
+    private String airplaneType;
+    private Timestamp departure;
+    private Timestamp arrival;
+
+    @Id
+    @Column(name = "ID")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "number")
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Basic
+    @Column(name = "airplane_type")
+    public String getAirplaneType() {
+        return airplaneType;
+    }
+
+    public void setAirplaneType(String airplaneType) {
+        this.airplaneType = airplaneType;
+    }
+
+    @Basic
+    @Column(name = "departure")
+    public Timestamp getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(Timestamp departure) {
+        this.departure = departure;
+    }
+
+    @Basic
+    @Column(name = "arrival")
+    public Timestamp getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(Timestamp arrival) {
+        this.arrival = arrival;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FlightEntity that = (FlightEntity) o;
+
+        if (id != that.id) return false;
+        if (number != that.number) return false;
+        if (airplaneType != null ? !airplaneType.equals(that.airplaneType) : that.airplaneType != null) return false;
+        if (departure != null ? !departure.equals(that.departure) : that.departure != null) return false;
+        if (arrival != null ? !arrival.equals(that.arrival) : that.arrival != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + number;
+        result = 31 * result + (airplaneType != null ? airplaneType.hashCode() : 0);
+        result = 31 * result + (departure != null ? departure.hashCode() : 0);
+        result = 31 * result + (arrival != null ? arrival.hashCode() : 0);
+        return result;
+    }
+}
---
+++ SEM1/DS/Assignment1_2/src/main/java/dao/UserEntity.java
@@ -0,0 +1,76 @@
+package dao;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user", schema = "assignment1_2", catalog = "")
+public class UserEntity {
+    private int id;
+    private String username;
+    private String password;
+    private String type;
+
+    @Id
+    @Column(name = "ID")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "type")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        if (id != that.id) return false;
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (type != null ? !type.equals(that.type) : that.type != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        return result;
+    }
+}
